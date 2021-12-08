b40cb23dd1dc9d089ae157e8adced8a051e218d2
Wayne Wang <nkgailei@gmail.com>
2021-12-08 23:30:00 +0800

    io.github.atwayne.num24.api

---
+++ api/src/main/java/io/github/atwayne/num24/api/CalculatorController.java
@@ -0,0 +1,26 @@
+package io.github.atwayne.num24.api;
+
+import io.github.atwayne.num24.core.Calculator;
+import io.github.atwayne.num24.core.ExpressionBuilder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/calculate")
+public class CalculatorController {
+
+    @GetMapping
+    public String Help() {
+        return "Hello!";
+    }
+
+    @PostMapping
+    public List<String> Calculate(@RequestBody List<Integer> input) {
+        var calculator = new Calculator();
+        var expressionBuilder = new ExpressionBuilder();
+        var solutions = calculator.Calculate(input);
+        var expressions = solutions.stream().map(x -> expressionBuilder.Build(x)).toList();
+        return expressions;
+    }
+}
